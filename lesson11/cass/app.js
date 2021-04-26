038e26d5679da3c530cca9115f2ad015b2ab7c01
gaiadg <gaia.digregorio@gmail.com>
2021-04-26 21:13:38 +0400

    cass

---
+++ lesson11/cass/app.js
@@ -0,0 +1,22 @@
+(function () {
+  'use script';
+
+  angular.module('feedApp', [])
+  .controller('feederApp', feederApp);
+
+  feederApp.$inject = ['$scope'];
+  function feederApp($scope) {
+    $scope.name = "Cass";
+    $scope.status = "hungry";
+
+    $scope.sayMessage = function () {
+      return "Pls gimme bacon";
+    };
+
+    $scope.givingFood = function () {
+      $scope.status = "fed";
+    };
+
+}
+
+})();
